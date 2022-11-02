d945bebaf0452f650f33df490e0750f64f2d544a
Travis Vachon <travis.vachon@gmail.com>
2022-11-02 12:28:32 -0700

    add value display widget

--- pages/index.js
+++ pages/index.js
@@ -1,7 +1,20 @@
+import { useState } from 'react'
 import { Plate } from '@udecode/plate-core';
 
 const editableProps = {
   placeholder: 'Type...',
 };
 
-export default () => <Plate editableProps={editableProps} />;
+export default function Home() {
+  const [currentValue, setCurrentValue] = useState()
+  return (
+    <div className="flex flex-row">
+      <div className="w-1/2">
+        <Plate editableProps={editableProps} onChange={setCurrentValue}/>
+      </div>
+      <pre className="w-1/2">
+        {currentValue && JSON.stringify(currentValue, null, 2)}
+      </pre>
+    </div>
+  )
+}
