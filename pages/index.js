84942a41e847e9e57c3a785c85416f4a0b61af94
Travis Vachon <travis.vachon@gmail.com>
2022-11-02 12:36:14 -0700

    add nodeId plugin
    bizarrely this behaves completely differently here than in garden. harumph.

--- pages/index.js
+++ pages/index.js
@@ -1,16 +1,24 @@
 import { useState } from 'react'
 import { Plate } from '@udecode/plate-core';
+import { createNodeIdPlugin } from '@udecode/plate-node-id'
 
 const editableProps = {
   placeholder: 'Type...',
 };
+
+const plugins = [
+  createNodeIdPlugin()
+]
 
 export default function Home() {
   const [currentValue, setCurrentValue] = useState()
   return (
     <div className="flex flex-row">
       <div className="w-1/2">
-        <Plate editableProps={editableProps} onChange={setCurrentValue}/>
+        <Plate
+          editableProps={editableProps}
+          onChange={setCurrentValue}
+          plugins={plugins} />
       </div>
       <pre className="w-1/2">
         {currentValue && JSON.stringify(currentValue, null, 2)}
